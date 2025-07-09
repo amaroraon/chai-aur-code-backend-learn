855c5f6c949ef1828026941fc325d8bc7b4b8ba1
Amar Oraon <amaroraon.dev@gmail.com>
2025-07-09 21:13:00 +0530

    connection to DB and Server setup

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+//configuring .env files
+dotenv.config({ path: "./.env" });
+
+//connection to database
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(
+        `Server running on address: http://localhost:${process.env.PORT}`
+      );
+    });
+
+    //listing to error while connecting app❌
+    app.on("error", (error) => {
+      throw new Error("ERR", error);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGO db connection FAILED !!!", error);
+  });
